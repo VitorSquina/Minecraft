b889473a36ffb026480fbd7e754226e46db50411
Vitor Squina <vitor122703@gmail.com>
2025-04-21 02:01:06 -0300

    Criando visualização da tela

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import { Physics } from '@react-three/cannon';
+import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { Sky } from '@react-three/drei'
-import { Physics } from '@react-three/cannon'
 import { Ground } from './components/Ground'
-import { Player } from './components/Player';
+import { Player } from './components/Player'
+import { FPV } from './components/FPV'
 
 function App() {
   return (
     <>
       <Canvas>
-        <Sky sunPosition={[100, 100, 20]} /> //CRIA O CEU e a possição do sol, deixando sempre dia
-        <ambientLight intesity={0.5}/> //Define a intensidade da luz ambiente
-
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <FPV />
         <Physics>
           <Player />
           <Ground />
         </Physics>
-
       </Canvas>
+      <div className='absolute centered cursor'>+</div>
     </>
   );
 }
