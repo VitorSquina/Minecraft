e1cd64b5f267de52bed846aee2e7918b0a85d585
Vitor Squina <vitor122703@gmail.com>
2025-04-21 01:27:57 -0300

    Criando textura e logica para p chão ainda sem gravidade

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import { Ground } from './components/Ground'
 
 function App() {
   return (
     <>
       <Canvas>
-        <Sky sunPosition={[100, 100, 20]} />
+        <Sky sunPosition={[100, 100, 20]} /> //CRIA O CEU e a possição do sol, deixando sempre dia
+        <ambientLight intesity={0.5}/> //Define a intensidade da luz ambiente
+
+        <Physics>
+          <Ground />
+        </Physics>
+
       </Canvas>
     </>
   );
--- src/images/textures.js
+++ src/images/textures.js
@@ -1,23 +1,34 @@
-import { TextureLoader } from "three";
+import { NearestFilter, TextureLoader, RepeatWrapping } from 'three'
 
 import {
 	dirtImg,
+	logImg,
 	grassImg,
 	glassImg,
-	woodImg,
-	logImg,
-} from "./images";
+	woodImg
+} from './images'
 
-const dirtTexturre = new TextureLoader(dirtImg)
-const logTexturre = new TextureLoader(dirtImg)
-const grassTexturre = new TextureLoader(dirtImg)
-const woodTexturre = new TextureLoader(dirtImg)
-const groundTexturre = new TextureLoader(dirtImg)
+const dirtTexture = new TextureLoader().load(dirtImg)
+const logTexture = new TextureLoader().load(logImg)
+const grassTexture = new TextureLoader().load(grassImg)
+const glassTexture = new TextureLoader().load(glassImg)
+const woodTexture = new TextureLoader().load(woodImg)
+const groundTexture = new TextureLoader().load(grassImg)
+
+dirtTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+grassTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+woodTexture.magFilter = NearestFilter;
+groundTexture.magFilter = NearestFilter;
+groundTexture.wrapS = RepeatWrapping
+groundTexture.wrapT = RepeatWrapping
 
 export {
-    dirtTexturre,
-    logTexturre,
-    grassTexturre,
-    woodTexturre,
-    groundTexturre
+	dirtTexture,
+	logTexture,
+	grassTexture,
+	glassTexture,
+	woodTexture,
+	groundTexture
 }
