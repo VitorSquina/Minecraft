475c49b9a787a46d348cc2ef457e7db0e3a7230a
Vitor Squina <vitor122703@gmail.com>
2025-04-21 02:36:55 -0300

    Incluindo o resto do menu e realizando alguns ajustes como o seletor de texturas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Canvas } from '@react-three/fiber';
 import { Ground } from './components/Ground'
 import { Player } from './components/Player'
 import { FPV } from './components/FPV'
-import { Cubes } from './components/Cubes';
+import { Cubes } from './components/Cubes'
+import { TextureSelector } from './components/TextureSelector';
+import { Menu } from './components/Menu';
 
 function App() {
   return (
@@ -15,11 +17,13 @@ function App() {
         <FPV />
         <Physics>
           <Player />
-          <Cubes/>
+          <Cubes />
           <Ground />
         </Physics>
       </Canvas>
       <div className='absolute centered cursor'>+</div>
+      <TextureSelector />
+      <Menu/>
     </>
   );
 }
